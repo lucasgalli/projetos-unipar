167faa5e6a7b6e2d6fdc90c56f57246474094566
Lucas Galli <lucasgalli.tsi@gmail.com>
2024-03-10 20:51:08 -0300

    aula dia 5, 6 e 7

--- 5-periodo/Projeto-busca-enderecos/scripts.js
+++ 5-periodo/Projeto-busca-enderecos/scripts.js
@@ -11,16 +11,34 @@ $(document).ready(function(){
 			return false;
 		}
 
-		$.ajax({
-			url: 'https://viacep.com.br/ws/'+cep+'/json/',
-			type: 'GET',
-			dataType: 'json',
-			success: function(data){
-				console.log(data);
-			}
-		})
-
+		loading();
+		setTimeout(function(){
+			$.ajax({
+				url: 'https://viacep.com.br/ws/'+cep+'/json/',
+				type: 'GET',
+				dataType: 'json',
+				success: function(data){
+					$('body').find('#resultados').html('<h2>Resultado da consulta</h2>'
+														+ '<p>CEP: '+data.cep+'</p>'
+														+ '<p>Logradouro: '+data.logradouro+'</p>'
+														+ '<p>Bairro: '+data.bairro+'</p>'
+														+ '<p>Cidade: '+data.localidade+'</p>'
+														+ '<p>Estado: '+data.uf+'</p>'
+														+ '<p>IBGE: '+data.ibge+'</p>'
+														);
+				}
+			})
+		}, 1000);
 
 	}
+
+	function loading(){
+		$('#resultados').html('<img src="loading.gif" />');
+	}
+
+	$('body').on('click', '#modo-noturno', function(){
+		alert(123);
+		$('body').toggleClass('dark-mode');
+	});
 
 })
