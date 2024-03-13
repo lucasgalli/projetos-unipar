fa28962a6ca48ccd8164116d31547ecf3ba653ef
Lucas Galli <lucasgalli.tsi@outlook.com>
2024-03-13 15:12:17 -0300

    Add files via upload

---
+++ 5-periodo/Projeto-lista/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+	function adicionaTarefa() {
+		var tarefa = $('#tarefa').val();
+		if (tarefa !== '') { //trim() - apagar os espaços
+			$('#lista-tarefas').prepend('<li class="item"><span class="descricao-tarefa">' + tarefa + '</span><i class="fa-solid fa-trash-can remover-tarefa"></i></li>');
+			$('#tarefa').val('');
+		}
+	}
+
+	$('body').on('keypress', '#tarefa', function (e) {
+		if (e.which === 13) {
+			adicionaTarefa();
+		}
+	});
+
+	$('body').on('click', '.remover-tarefa', function () {
+		$(this).parent().remove();
+	});
+
+	$('body').on('click', '.item', function () {
+		$(this).find('.descricao-tarefa').toggleClass('feito');
+	});
+
+	$(".item").draggable({
+		revert: true
+	});
+	$("#lista-tarefas").sortable({
+		revert: true
+	});
+	$(".item").draggable({
+		connectToSortable: "#sortable",
+		helper: "clone",
+		revert: "invalid"
+	});
+	$("ul, li").disableSelection();
+
+	$('input[name=tarefa').focus();
+});
