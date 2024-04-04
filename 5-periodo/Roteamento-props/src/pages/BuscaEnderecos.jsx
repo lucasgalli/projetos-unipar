305a95db4eda6e5aca0412d4f364fcdf29cc2db8
Lucas Galli <lucasgalli.tsi@gmail.com>
2024-04-04 20:01:36 -0300

    ultimas do enderecos

--- 5-periodo/Roteamento-props/src/pages/BuscaEnderecos.jsx
+++ 5-periodo/Roteamento-props/src/pages/BuscaEnderecos.jsx
@@ -2,17 +2,51 @@ import React, { useState } from "react";
 import { UrlViaCep } from "../config/config";
 
 const BuscaEnderecos = () => {
-	const [cep, setCep] = useState("");
-	console.log(cep)
-	const response = UrlViaCep.get(`${cep}/json/`).then(function (response) {
-		console.log(response);
+	const [endereco, setEndereco] = useState({
+		cep: "",
 	});
 
+	function buscaCep(e) {
+		const cep = e.target.value;
+		setEndereco({
+			cep,
+		});
+
+		if (cep && cep.length === 8) {
+			try {
+				UrlViaCep.get(`${cep}/json/`).then(function (response) {
+					const dados = response.data;
+					if (!dados.erro) {
+						console.log(dados);
+						setEndereco({
+							cep: cep,
+							rua: dados.logradouro,
+							bairro: dados.bairro,
+							cidade: dados.localidade,
+							estado: dados.uf,
+						});
+					}
+				});
+			} catch (error) {}
+		}
+	}
+
 	return (
-		<>
-			
-			<input type="text" onChange={(e) => setCep(e.target.value)} />
-		</>
+		<main>
+			<h3>Digite o cep</h3>
+			<input type="text" onChange={buscaCep} maxLength={8} />
+			<div></div>
+			<section>
+				<h3></h3>
+				<ul>
+					<li>{endereco.cep}</li>
+					<li>{endereco.rua}</li>
+					<li>{endereco.bairro}</li>
+					<li>{endereco.cidade}</li>
+					<li>{endereco.estado}</li>
+				</ul>
+			</section>
+		</main>
 	);
 };
 
